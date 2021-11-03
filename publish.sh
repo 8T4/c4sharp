dd1b4e3d4a4ebc80a38b99c795fe0c35f6876e1d
yanjustino <contato@yanjustino.com>
2021-11-03 01:13:45 -0300

    add

--- publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-VERSION="2.2.2"
-NUGET_KEY="oy2fixxyzuqmc5w5eo2qcfyotdglykxufejegfb3rzxfgi"
-NUGET_INDEX="https://api.nuget.org/v3/index.json"
-BUILD_TYPE="Release"
-
-dotnet build --configuration $BUILD_TYPE
-dotnet pack --no-restore --configuration $BUILD_TYPE -p:PackageVersion=$VERSION
-dotnet nuget push "./src/C4Sharp/bin/${BUILD_TYPE}/C4Sharp.${VERSION}.nupkg" --api-key $NUGET_KEY --source $NUGET_INDEX
