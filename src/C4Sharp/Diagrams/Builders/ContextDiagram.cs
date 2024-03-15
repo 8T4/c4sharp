60d1adb8ddc890ccbeff27ed096ab7502ccfde42
yanjustino <yanlimaj@gmail.com>
2024-03-15 13:29:12 -0300

    Fixing ContextDiagram.cs

--- src/C4Sharp/Diagrams/Builders/ContextDiagram.cs
+++ src/C4Sharp/Diagrams/Builders/ContextDiagram.cs
@@ -6,7 +6,7 @@ namespace C4Sharp.Diagrams.Builders;
 public abstract class ContextDiagram: DiagramBuilder
 {
     protected override string Title { get; } = "Context Diagram";
-    protected override DiagramType DiagramType { get; } = DiagramType.Component;
+    protected override DiagramType DiagramType { get; } = DiagramType.Context;
     
     protected EnterpriseBoundary Bound(string alias, string label, params Structure[] structures) =>
         new(alias, label, structures);
